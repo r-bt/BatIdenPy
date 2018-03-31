3b295a19b3fb03afb11a26d300057e2d8166ce1c
Richard Beattie <richard.beattie@aol.co.uk>
2018-03-31 15:22:43 +0100

    Added function to validate input

--- BatCall.py
+++ BatCall.py
@@ -1,17 +1,23 @@
-import requests
+import requests, datetime, re
 
 class BatCall(object):
 
     def __init__(self, filepath, date_recorded, location):
         self.filepath = filepath
-        self.data_recorded = date_recorded
+        self.date_recorded = date_recorded
         self.location = location
+
+    def validate(self):
+        try:
+            datetime.datetime.strptime(self.date_recorded, "%Y-%m-%d %H:%M:%S")
+        except:
+            return 0
+        if(re.match("^-?([1-8]\d\.\d+|90), -?([0-1]?[0-7]\d\.\d+|180)$", self.location) == None):
+            return 2
+        return 1
 
     def upload(self):
         files = {"bat_call": open(self.filepath, 'rb')}
-        data = {"date_recorded": self.data_recorded, "location": self.location}
-        r = requests.post("http://localhost/BatIndentificationProject/upload_audio.php", files=files, data=data)
+        data = {"date_recorded": self.date_recorded, "location": self.location}
+        r = requests.post("http://localhost/BatIndentificationProject/BatIdentificationServer/upload_audio.php", files=files, data=data)
         return(r.status_code)
-
-tmpCall = BatCall("audio.mp3", "2017-11-12 11:44:22", "40.741895, -73.989308")
-print(tmpCall.upload())
